1a4e0a93e097d43ef82186a65df1232891e82a30
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-06-09 00:32:35 +0600

    two problem solved

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello")
+function formatString(input: string, toUpper?: boolean): string{
+    if(toUpper === false){
+        return input.toLowerCase()
+    }else{
+        return input.toUpperCase()
+    }
+}
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+  const Filtered_By_Rating_Items: { title: string; rating: number }[] = []
+  
+    for(let i=0; i< items.length; i++){
+        if(items[i].rating >=4){
+            Filtered_By_Rating_Items.push(items[i])
+        }
+    }
+    return Filtered_By_Rating_Items;
+}
